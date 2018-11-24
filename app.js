6f466cd93a66575841aab2af8d05e4bd635ddb2c
alexgimson86 <alexgimson@gmail.com>
2018-11-23 23:12:09 -0600

    node todo app in process still, got delete, and insert and display working. need to do make edit option

---
+++ app.js
@@ -0,0 +1,61 @@
+var express = require('express')
+var app = express()
+const MongoClient = require('mongodb').MongoClient
+const assert = require('assert');
+const url = 'mongodb://127.0.0.1:27017/'
+var bodyParser = require("body-parser")
+var expressMongoDb = require('express-mongo-db');
+app.use(expressMongoDb('mongodb://localhost/test'));
+var count = 3
+var updatedTodo;
+
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+var todoArray = [];
+
+app.get("/", function(req, res) {
+    const db = req.db
+
+    db.collection('todo').find({}).toArray(function(err, result) {
+        if (err) throw err
+        updatedTodo = result
+        res.render("home.ejs", { todoArray: updatedTodo })
+    });
+});
+app.get("/results", function(req, res) {
+    const db = req.db
+
+    if (req.query.todo != null) {
+        var obj = { id: count, to_do: req.query.todo }
+        count++
+        db.collection('todo').insertOne(obj)
+            .then(function() {
+                db.collection('todo').find({}).toArray(function(err, result) {
+                    if (err) throw err
+                    res.render("results.ejs", { todoArray: result })
+
+                })
+            })
+    }
+});
+app.get("/delete", function(req, res) {
+    const db = req.db
+    todoArray = req.query.check
+
+    //check if array or error could get thrown
+    if (Array.isArray(todoArray)) {
+        todoArray.forEach(element => {
+            db.collection('todo').deleteMany({ to_do: element })
+        });
+    } else {
+        db.collection('todo').deleteMany({ to_do: todoArray })
+    }
+    db.collection('todo').find({}).toArray(function(err, result) {
+
+        res.render("delete.ejs", { todoArray: result })
+    })
+});
+app.listen(3000, function() {
+    console.log("App is listening on port 3000")
+});
